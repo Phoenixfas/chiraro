4803b74613fb94aa2b7ce5f65ce9b2ba49dc305d
Fasika Dagnachew <fasika1515@gmail.com>
2023-12-26 01:50:13 +0300

    home services section

--- app/ServicesCards.tsx
+++ app/ServicesCards.tsx
@@ -11,7 +11,7 @@ export default function ServicesCards() {
                 <div className="text-[6em] relative z-10 w-[140px] h-[120px] bg-[#080C1B] flex items-start justify-center rounded-b-[100px] shadow-[0_15px_0_#00000033,inset_0_-8px_0_#fff] before:content-[''] before:w-[50px] before:h-[50px] before:absolute before:z-[-1] before:top-0 before:left-[-50px] before:bg-transparent before:rounded-tr-[50px] before:shadow-[15px_-15px_0_15px_#080C1B] after:content-[''] after:w-[50px] after:h-[50px] after:absolute after:z-[-1] after:top-0 after:right-[-50px] after:bg-transparent after:rounded-tl-[50px] after:shadow-[-15px_-15px_0_15px_#080C1B]"><BsGlobe2 /></div>
                 <div className="w-full px-[30px] text-center flex flex-col items-center gap-[15px]">
                     <h2 className="text-[1.8em] font-bold leading-[2rem]">Web Development</h2>
-                    <p className="leading-[1.5em]">Experience unmatched web development, tailored sites, and seamless user experiences. Your digital success begins here—crafted uniquely for your brand's online journey.</p>
+                    <p className="leading-[1.5em]">Experience unmatched web development, tailored sites, and seamless user experiences. Your digital success begins here—crafted uniquely for your brand&apos;s online journey.</p>
                 </div>
             </div>
 
