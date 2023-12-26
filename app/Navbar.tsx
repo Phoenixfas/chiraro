0de8967848eb7c896208f6e3f310bae0e1d6864a
Fasika Dagnachew <fasika1515@gmail.com>
2023-12-26 23:36:29 +0300

    footer

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -30,8 +30,8 @@ export default function Navbar() {
     }, [])
 
   return (
-    <div className="w-full flex flex-col items-center">
-        <div className={`w-[95%] duration-300 fixed ${scrollingUp ? 'top-5' : 'top-0'} bg-[rgba(255,255,255,.05)] backdrop-blur-sm flex items-center justify-between px-8 py-3 border-[2px] border-[#192655] rounded-2xl`}>
+    <div className="w-full flex flex-col items-center relative z-[999999999]">
+        <div className={`w-[95%] duration-300 fixed ${scrollingUp ? 'top-5' : 'top-0'} bg-[rgba(255,255,255,.05)] backdrop-blur-md flex items-center justify-between px-8 py-3 border-[2px] border-[#192655] rounded-2xl`}>
             <Link href={"/"} className='flex items-center text-[25px] tracking-[.4rem]'>
                 <p className=''>CH</p>
                 <Image src="/logo.svg" alt="logo" width="30" height="45" className='w-auto h-[45px] object-contain mr-1'/>
@@ -39,10 +39,10 @@ export default function Navbar() {
             </Link>
 
             <div className="flex items-center gap-14 text-base">
-                <Link href={"/services"} className='hover:text-[#aaa] duration-300'>Services</Link>
-                <Link href={"/"} className='hover:text-[#aaa] duration-300'>Pricing</Link>
-                <Link href={"/"} className='hover:text-[#aaa] duration-300'>About</Link>
-                <Link href={"/"} className='hover:text-[#aaa] duration-300'>Contact</Link>
+                <Link href={"/services"} className='hover:text-[#01BAEF] duration-300'>Services</Link>
+                <Link href={"/"} className='hover:text-[#01BAEF] duration-300'>Pricing</Link>
+                <Link href={"/"} className='hover:text-[#01BAEF] duration-300'>About</Link>
+                <Link href={"/"} className='hover:text-[#01BAEF] duration-300'>Contact</Link>
             </div>
         </div>
     </div>
