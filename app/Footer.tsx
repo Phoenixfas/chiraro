87547e17f5476ad3cd30c1bbc3778494fbaec674
Fasika Dagnachew <fasika1515@gmail.com>
2024-02-05 10:47:11 +0300

    footer change

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -3,7 +3,7 @@ import { FaInstagram, FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa"
 
 export default function Footer() {
   return (
-    <footer className="relative w-full bg-[linear-gradient(to_top,_#192655,_#01BAEF)] min-h-[100px] p-[20px_0_0] flex justify-center items-center flex-col mt-[200px]">
+    <footer className="relative w-full bg-[#01baef] min-h-[100px] p-[20px_0_0] flex justify-center items-center flex-col mt-[200px]">
         <div className="waves">
             <div className="absolute top-[-99px] left-0 w-full h-[100px] bg-[url('/images/wave.png')] bg-[length:1000px_100px] z-10 opacity-100 bottom-0 animate-animateWaves-4" id="wave1"></div>
             <div className="absolute top-[-99px] left-0 w-full h-[100px] bg-[url('/images/wave.png')] bg-[length:1000px_100px] z-9 opacity-50 bottom-[10px] animate-animateWaves-r-4" id="wave2"></div>
@@ -30,7 +30,7 @@ export default function Footer() {
             <Link className="text-white hover:text-[#192655] duration-300" href="/about">About</Link>
             <Link className="text-white hover:text-[#192655] duration-300" href="/contact">Contact</Link>
         </div>
-        <div className="w-full p-[15px_20px] text-[0.9rem] text-center bg-[#192655]">
+        <div className="w-full p-[15px_20px] text-[0.9rem] text-center ">
             <p>&copy; 2023 Chiraro Digital Solutions. All Rights Reserved.</p>
         </div>
     </footer>
